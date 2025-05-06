1c67fc21cfb1874f0e15531d461dbd94f0fca099
Barathkumar <140275835+jkbarathkumar@users.noreply.github.com>
2025-05-06 12:00:14 +0530

    Create create_resource_group.tf

---
+++ create_resource_group.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+        source = "hashicorp/azurerm"
+        version = "~> 4.8.0"
+    }
+  }
+  required_version = ">=1.9.0"
+}
+
+provider "azurerm" {
+    features { 
+    }
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "barath"
+  location = "West Europe"
+}
